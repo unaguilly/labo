98bc76d124a4c15c35a762fb626b2ff9f473786f
unaguilly <unagui@gmail.com>
2022-11-19 22:12:11 +0000

    ahora con mas ram

--- src/competencia4/agos/z941_HT_lightgbm_V10.R
+++ src/competencia4/agos/z941_HT_lightgbm_V10.R
@@ -19,9 +19,9 @@ require("mlrMBO")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "HT9410_10"
+PARAM$experimento <- "HT9410_agos"
 
-PARAM$exp_input  <- "TS9310_10"
+PARAM$exp_input  <- "TS9310_agos"
 # FIN Parametros del script
 
 
@@ -33,7 +33,7 @@ options(error = function() {
 })
 #------------------------------------------------------------------------------
 
-ksemilla  <- 238001
+ksemilla  <- 239069
 
 kcrossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
@@ -79,7 +79,7 @@ hs <- makeParamSet(
 
 
 #si usted es ambicioso, y tiene paciencia, podria subir este valor a 100
-kBO_iteraciones  <- 50  #iteraciones de la Optimizacion Bayesiana
+kBO_iteraciones  <- 10  #iteraciones de la Optimizacion Bayesiana
 
 #------------------------------------------------------------------------------
 #graba a un archivo los componentes de lista
